e70da4f409d4f2cde19c32c26340ea507082ad82
fsRoger <fsroger@outlook.com>
2024-04-03 11:16:57 -0300

    Conteúdo Atualizado após a Entrega do dia 01/04 react-hot-toast

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,18 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-hot-toast']
-    }
-  }
+      external: ['react-hot-toast'],
+      output: {
+        manualChunks: {
+          'react-hot-toast': ['react-hot-toast'],
+        },
+      },
+    },
+
+    resolve: {
+      alias: {
+        'react-hot-toast': './module.js',
+      },
+    },
+  },
 });
